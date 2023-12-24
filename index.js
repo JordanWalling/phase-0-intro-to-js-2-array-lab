0c15d0bb907d4242f8412ce28f2e3f89fb0d4653
JordanWalling <jordanwalling1991@gmail.com>
2023-12-24 21:15:33 +1100

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  return cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+  return cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
+}
+
+function appendCat(name) {
+  const newArray = [...cats, name];
+  return newArray;
+}
+
+function prependCat(name) {
+  const newArray = [name, ...cats];
+  return newArray;
+}
+
+function removeLastCat() {
+  const newArray = cats.slice(0, 2);
+  return newArray;
+}
+
+function removeFirstCat() {
+  const newArray = cats.slice(1);
+  return newArray;
+}
